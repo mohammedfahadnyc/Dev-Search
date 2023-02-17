3c8e84eaf2e691715c0f58cd5f6af4467359ff1d
mohammedfahadnyc <fahadmohammed29299@gmail.com>
2023-02-16 22:47:50 -0500

    Added Index  For Developers

--- users/models.py
+++ users/models.py
@@ -10,7 +10,7 @@ class Profile(models.Model):
     short_intro = models.CharField(max_length=200,null=True,blank=True)
     bio = models.TextField(null=True,blank=True)
     username = models.CharField(max_length=255,null=True,blank=True)
-    profile_image = models.ImageField(null=True,blank=True,upload_to='profiles/',default='profiles/user-default.png')
+    profile_image = models.ImageField(null=True,blank=True,upload_to='profiles/',default="images/profile-pics/user-default.png")
     social_github = models.CharField(max_length=200,null=True,blank=True)
     social_linkedin = models.CharField(max_length=200, null=True, blank=True)
     social_instagram = models.CharField(max_length=200, null=True, blank=True)
@@ -21,6 +21,19 @@ class Profile(models.Model):
 
     def __str__(self):
         return str(self.name)
+
+class Skill(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    name = models.CharField(max_length=100,blank=True,null=True)
+    description = models.TextField(max_length=500,blank=True,null=True)
+    owner = models.ForeignKey(Profile,null=True,blank=True,on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return str(self.name)
+
+
+
 
 
 
